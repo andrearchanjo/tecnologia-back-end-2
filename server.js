da466f0f02a37ec3a87c501af1bcf1b257d6a94a
André Coelho <archanjoandre@yahoo.com.br>
2024-06-02 09:58:15 -0300

    Incrementando server.js e adicionando páginas.

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const http = require('http');
-const url = require('url'); //permite "resolver" e fazer parser de uma url
-const fs = require('fs'); //interação com sistemas de arquivos
-const path = require('path'); //lidar com caminho de arquivos, extensões
+const url = require('url'); // Permite "resolver" e fazer parser de uma URL
+const fs = require('fs'); // Interação com sistemas de arquivos
+const path = require('path'); // Lidar com caminho de arquivos, extensões
 
 const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
 const CONTENT_TYPE = 'Content-Type';
-const TEXT_PLAIN = 'text/plain';
-const LOCATION = 'location';
+const TEXT_PLAIN = 'text/plain; charset=utf-8';
+const LOCATION = 'Location';
 const DEFAULT_LOCATION = 'index.html';
-const RESPONSE_MESSAGE = "OK!";
-const RESPONSE_MESSAGE_NOT_FOUND = `404: Arquivo não encontrado!`;
-const RESPONSE_MESSAGE_INTERNAL_SERVER_ERROR = `500: Erro interno do servidor!`;
+const NOT_FOUND_LOCATION = '/pages/not-found.html';
+const RESPONSE_MESSAGE_INTERNAL_SERVER_ERROR = '500: Erro interno do servidor!';
 const SERVER_RUNNING_MESSAGE = `O servidor está sendo executado em http://${HOSTNAME}:${PORT}/`;
 
 const mimeTypes = {
@@ -20,8 +19,10 @@ const mimeTypes = {
     js: "text/javascript",
     png: "image/png",
     jpeg: "image/jpeg",
-    jpg: "image/jpg",
-    woff: "font/woof",
+    jpg: "image/jpeg",
+    woff: "font/woff",
+    pdf: "application/pdf",
+    mp4: "video/mp4"
 };
 
 http.createServer((req, res) => {
@@ -29,37 +30,46 @@ http.createServer((req, res) => {
     let caminho_completo_recurso = path.join(process.cwd(), decodeURI(acesso_uri));
     console.log(caminho_completo_recurso);
 
-    let recurso_carregado;
-    try {
-        recurso_carregado = fs.lstatSync(caminho_completo_recurso);
-    } catch (error) {
-        res.writeHead(404, {
-            [CONTENT_TYPE]: TEXT_PLAIN
-        });
-        res.write(RESPONSE_MESSAGE_NOT_FOUND);
-        res.end();    
-    }
+    // Assíncrono ao invés de síncrono
+    fs.lstat(caminho_completo_recurso, (err, stats) => {
+        if (err) {
+            // Recurso não encontrado
+            if (err.code === 'ENOENT') {
+                // Redirecionamento temporário
+                res.writeHead(302, {
+                    [LOCATION]: NOT_FOUND_LOCATION
+                });
+                res.end();
+            } else {
+                res.writeHead(500, {
+                    [CONTENT_TYPE]: TEXT_PLAIN
+                });
+                res.write(RESPONSE_MESSAGE_INTERNAL_SERVER_ERROR);
+            }
+            res.end();
+            return;
+        }
 
-    if (recurso_carregado.isFile()) {
-        let mimeType = mimeTypes[path.extname(caminho_completo_recurso).substring(1)];
-
-        res.writeHead(200, {
-            [CONTENT_TYPE]: mimeType
-        });
-        let fluxo_arquivo = fs.createReadStream(caminho_completo_recurso);
-        fluxo_arquivo.pipe(res); 
-    } else if (recurso_carregado.isDirectory()) {
-        res.writeHead(302, {
-            [LOCATION]: DEFAULT_LOCATION
-        });
-        res.end();
-    } else {
-        res.writeHead(500, {
-            [CONTENT_TYPE]: TEXT_PLAIN
-        });
-        res.write(RESPONSE_MESSAGE_INTERNAL_SERVER_ERROR);
-        res.end();
-    }
+        if (stats.isFile()) {
+            let mimeType = mimeTypes[path.extname(caminho_completo_recurso).substring(1)] || TEXT_PLAIN;
+            res.writeHead(200, {
+                [CONTENT_TYPE]: mimeType
+            });
+            let fluxo_arquivo = fs.createReadStream(caminho_completo_recurso);
+            fluxo_arquivo.pipe(res);
+        } else if (stats.isDirectory()) {
+            res.writeHead(302, {
+                [LOCATION]: DEFAULT_LOCATION
+            });
+            res.end();
+        } else {
+            res.writeHead(500, {
+                [CONTENT_TYPE]: TEXT_PLAIN
+            });
+            res.write(RESPONSE_MESSAGE_INTERNAL_SERVER_ERROR);
+            res.end();
+        }
+    });
 }).listen(PORT, HOSTNAME, () => {
     console.log(SERVER_RUNNING_MESSAGE);
 });
